3767eeca76d3d1e037a2abdfec29638f9acb2ce0
syf107 <syariefarifin107@gmail.com>
2022-03-12 23:00:20 +0700

    adding click event learning and useState

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+  // let name = 'mario';
+  const [name, setName] = useState("mario");
+  const [age, setAge] = useState(30);
+
+  const handleClick = () => {
+    setName("luigi");
+    setAge(25);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old.
+      </p>
+      <button onClick={handleClick}>Click me!</button>
     </div>
   );
 };
