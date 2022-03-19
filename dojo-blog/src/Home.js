5305aec88dc35b781073c891253a39437a1f5145
syf107 <syariefarifin107@gmail.com>
2022-03-19 23:45:20 +0700

    adding more react learning

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,22 +1,19 @@
-import { useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const Home = () => {
-  // let name = 'mario';
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(30);
-
-  const handleClick = () => {
-    setName("luigi");
-    setAge(25);
-  };
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old.
-      </p>
-      <button onClick={handleClick}>Click me!</button>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
---
+++ dojo-blog/src/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error("Couldn't fetch the data because of the resource.");
+          }
+
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
